951460a596679998d80a3811c483cf5401e27313
masal-98 <100738621+masal-98@users.noreply.github.com>
2023-08-21 16:22:00 +0200

    Add files via upload

---
+++ Sleep_position_classifier_PSoC.cydsn/ErrorCodes.h
@@ -0,0 +1,28 @@
+/* ===============================================
+ *
+ * Alfonzo Massimo, Canavate Chloé, Franke Patrick
+ * Project 05 - Sleep Position Classifier
+ * Electronic Technologies and Biosensor Laboratory
+ * Academic year 2021 - 2022
+ * ErrorCodes.h file
+ * \brief Error codes definition
+ *
+ * =============================================== */
+
+
+/**
+*   \brief Error codes.
+* 
+*   This definition defines several error codes that will
+*   be used throughout the project.
+*/
+#ifndef __ERRORCODES_H
+    #define __ERRORCODES_H
+    
+    typedef enum {
+        NO_ERROR,           ///< No error generated
+        ERROR               ///< Error generated
+    } ErrorCode;
+
+#endif
+/* [] END OF FILE */
